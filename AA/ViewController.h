ed4af8a480d335af33811281e93ab590e13d42d5
Chen Yong <290363831@qq.com>
2017-06-26 10:22:33 +0800

    add files

---
+++ AA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AA
+//
+//  Created by ucmed on 2017/6/26.
+//  Copyright © 2017年 ucmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ upload.sh
@@ -0,0 +1,1 @@
+pod repo push CYPrivatePods AA.podspec --verbose --allow-warnings
